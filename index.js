cb376435c129dca0c89468bb7527bf011550835b
CGM123sherry <g.christinemurithi@gmail.com>
2024-07-22 12:18:00 +0300

    completed the sending data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const { response } = require("express");
+
+function submitData(userName, userEmail) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((responseObject) => {
+      let p = document.createElement("p");
+      p.textContent = responseObject.id;
+      document.body.appendChild(p);
+    })
+    .catch((error) => {
+      let p = document.createElement("p");
+      p.textContent = error.message;
+      document.body.appendChild(p);
+    });
+}
+console.log(submitData());
